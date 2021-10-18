a7eddcebf68e7b6d445bab9bbad4e7e7297c5094
metvsk <68542818+metvsk@users.noreply.github.com>
2021-10-18 09:44:18 +0530

    Updated Icon

--- app/src/main/java/com/chillandcode/diceroller/MainActivity.kt
+++ app/src/main/java/com/chillandcode/diceroller/MainActivity.kt
@@ -2,10 +2,21 @@ package com.chillandcode.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String="Main Activity : "
+    val diceRange:IntRange=1..6
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rollDice()
+    }
+
+
+    private fun rollDice(){
+        val randomNumber=diceRange.random()
+        Log.i(TAG, "rollDice: dice value is : $randomNumber")
     }
 }
